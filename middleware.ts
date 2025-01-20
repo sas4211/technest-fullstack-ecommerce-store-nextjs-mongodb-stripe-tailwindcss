cfb5e5c315b947d49854ba19f07acd9fe6302ab2
sas4211 <sas4211@gmail.com>
2025-01-20 17:18:01 +0500

    initial commit

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: ["/:path*"]
+});
+
+export const config = {
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',],
+};
